4ce84b71269570a8a69600bcc7e53661411cf6e0
Kevin Zhang <kzhang@federatedwireless.com>
2017-08-01 14:41:32 -0400

    rename osx layer

--- keyboard/hhkb/keymap_personal.c
+++ keyboard/hhkb/keymap_personal.c
@@ -24,7 +24,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
            LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,FN0,             \
                 LGUI,LALT,          SPC,                RALT,FN1),
 
-    /* Layer 1: Mac OSX Layer
+    /* Layer 1: OS X Mode
      * ,-----------------------------------------------------------.
      * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|  \|  `|
      * |-----------------------------------------------------------|
